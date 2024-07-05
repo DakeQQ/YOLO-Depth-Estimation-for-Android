3a612b11d5976b67f51ecdd89155882158e18365
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-07-05 16:46:15 +0800

    Update YOLO_NAS_Export.py

--- Export_ONNX/YOLO_NAS/YOLO_NAS_Export.py
+++ Export_ONNX/YOLO_NAS/YOLO_NAS_Export.py
@@ -9,18 +9,12 @@ output_path = "./yolo_nas_s.onnx"
 super_gradients_path = "C:/Users/dake/.conda/envs/python_39/Lib/site-packages/super_gradients/"  # Specify the path where the python package "super_gradients" stored.
 
 # Replace the original source code file.
-path_0 = super_gradients_path + "modules/qarepvgg_block.py"
-path_1 = super_gradients_path + "training/models/detection_models/yolo_nas/yolo_stages.py"
-path_2 = super_gradients_path + "training/models/detection_models/customizable_detector.py"
-path_3 = super_gradients_path + "training/models/detection_models/yolo_nas/dfl_heads.py"
-path_4 = super_gradients_path + "training/utils/bbox_utils.py"
-path_5 = super_gradients_path + "training/models/detection_models/yolo_nas/panneck.py"
-shutil.copyfile(modified_path + "qarepvgg_block.py", path_0)
-shutil.copyfile(modified_path + "yolo_stages.py", path_1)
-shutil.copyfile(modified_path + "customizable_detector.py", path_2)
-shutil.copyfile(modified_path + "dfl_heads.py", path_3)
-shutil.copyfile(modified_path + "bbox_utils.py", path_4)
-shutil.copyfile(modified_path + "panneck.py", path_5)
+shutil.copyfile(modified_path + "qarepvgg_block.py", super_gradients_path + "modules/qarepvgg_block.py")
+shutil.copyfile(modified_path + "yolo_stages.py", super_gradients_path + "training/models/detection_models/yolo_nas/yolo_stages.py")
+shutil.copyfile(modified_path + "customizable_detector.py", super_gradients_path + "training/models/detection_models/customizable_detector.py")
+shutil.copyfile(modified_path + "dfl_heads.py", super_gradients_path + "training/models/detection_models/yolo_nas/dfl_heads.py")
+shutil.copyfile(modified_path + "bbox_utils.py", super_gradients_path + "training/utils/bbox_utils.py")
+shutil.copyfile(modified_path + "panneck.py", super_gradients_path + "training/models/detection_models/yolo_nas/panneck.py")
 shutil.copyfile(config_path, modified_path + config_path)
 
 from super_gradients.training import models
