1d38d8c6531fb9c498a19262cc3304043aaf17f9
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-07-07 13:12:03 +0800

    Add files via upload

---
+++ Export_ONNX/YOLO_NAS/modeling_modified/yolo_stages.py
@@ -0,0 +1,383 @@
+from functools import partial
+from typing import Type, List, Iterable, Union, Optional, Callable
+
+import torch
+from super_gradients.common import UpsampleMode
+from super_gradients.common.factories.type_factory import TypeFactory
+from super_gradients.modules.sampling import make_upsample_module_with_explicit_channels
+from super_gradients.training.utils.regularization_utils import DropPath
+from torch import nn, Tensor
+
+from super_gradients.common.registry import register_detection_module
+from super_gradients.modules import Residual, BaseDetectionModule
+from super_gradients.common.decorators.factory_decorator import resolve_param
+from super_gradients.common.factories.activations_type_factory import ActivationsTypeFactory
+from super_gradients.modules import QARepVGGBlock, Conv
+from super_gradients.modules.utils import width_multiplier
+from super_gradients.module_interfaces import SupportsReplaceInputChannels
+
+
+__all__ = ["YoloNASStage", "YoloNASUpStage", "YoloNASStem", "YoloNASDownStage", "YoloNASBottleneck"]
+
+
+class YoloNASBottleneck(nn.Module):
+    """
+    A bottleneck block for YoloNAS. Consists of two consecutive blocks and optional residual connection.
+    """
+
+    def __init__(
+        self,
+        input_channels: int,
+        output_channels: int,
+        block_type: Type[nn.Module],
+        activation_type: Type[nn.Module],
+        shortcut: bool,
+        use_alpha: bool,
+        drop_path_rate: float = 0.0,
+    ):
+        """
+        Initialize the YoloNASBottleneck block
+
+        :param input_channels: Number of input channels
+        :param output_channels: Number of output channels
+        :param block_type: Type of the convolutional block
+        :param activation_type: Activation type for the convolutional block
+        :param shortcut: If True, adds the residual connection from input to output.
+        :param use_alpha: If True, adds the learnable alpha parameter (multiplier for the residual connection).
+        :param drop_path_rate: Drop path rate for the residual path of the block
+        """
+        super().__init__()
+
+        self.cv1 = block_type(input_channels, output_channels, activation_type=activation_type)
+        self.cv2 = block_type(output_channels, output_channels, activation_type=activation_type)
+        self.add = shortcut and input_channels == output_channels
+        self.shortcut = Residual() if self.add else None
+        self.drop_path = DropPath(drop_path_rate) if drop_path_rate > 0.0 else nn.Identity()
+        if use_alpha:
+            self.alpha = torch.nn.Parameter(torch.tensor([1.0]), requires_grad=True)
+        else:
+            self.alpha = 1.0
+
+    def forward(self, x):
+        y = self.drop_path(self.cv2(self.cv1(x)))
+        return self.alpha * self.shortcut(x) + y if self.add else y
+
+
+class SequentialWithIntermediates(nn.Sequential):
+    """
+    A Sequential module that can return all intermediate values as a list of Tensors
+    """
+
+    def __init__(self, output_intermediates: bool, *args):
+        super(SequentialWithIntermediates, self).__init__(*args)
+        self.output_intermediates = output_intermediates
+
+    def forward(self, input: Tensor) -> List[Tensor]:
+        if self.output_intermediates:
+            output = [input]
+            for module in self:
+                output.append(module(output[-1]))
+            return output
+        #  For uniformity, we return a list even if we don't output intermediates
+        return [super(SequentialWithIntermediates, self).forward(input)]
+
+
+class YoloNASCSPLayer(nn.Module):
+    """
+    Cross-stage layer module for YoloNAS.
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        num_bottlenecks: int,
+        block_type: Type[nn.Module],
+        activation_type: Type[nn.Module],
+        shortcut: bool = True,
+        use_alpha: bool = True,
+        expansion: float = 0.5,
+        hidden_channels: int = None,
+        concat_intermediates: bool = False,
+        drop_path_rates: Union[Iterable[float], None] = None,
+        dropout_rate: float = 0.0,
+    ):
+        """
+
+        :param in_channels: Number of input channels.
+        :param out_channels:  Number of output channels.
+        :param num_bottlenecks: Number of bottleneck blocks.
+        :param block_type: Bottleneck block type.
+        :param activation_type: Activation type for all blocks.
+        :param shortcut: If True, adds the residual connection from input to output.
+        :param use_alpha: If True, adds the learnable alpha parameter (multiplier for the residual connection).
+        :param expansion: If hidden_channels is None, hidden_channels is set to in_channels * expansion.
+        :param hidden_channels: If not None, sets the number of hidden channels used inside the bottleneck blocks.
+        :param concat_intermediates:
+        :param drop_path_rates: List of drop path probabilities for each bottleneck block.
+                                Must have the length equal to the num_bottlenecks or None.
+        :param dropout_rate: Dropout probability before the last convolution in this layer.
+        """
+        if drop_path_rates is None:
+            drop_path_rates = [0.0] * num_bottlenecks
+        else:
+            drop_path_rates = tuple(drop_path_rates)
+        if len(drop_path_rates) != num_bottlenecks:
+            raise ValueError(
+                f"Argument drop_path_rates ({drop_path_rates}, len {len(drop_path_rates)} "
+                f"must have the length equal to the num_bottlenecks ({num_bottlenecks})."
+            )
+
+        super(YoloNASCSPLayer, self).__init__()
+        if hidden_channels is None:
+            hidden_channels = int(out_channels * expansion)
+        self.conv1 = Conv(in_channels, hidden_channels, 1, stride=1, activation_type=activation_type)
+        self.conv2 = Conv(in_channels, hidden_channels, 1, stride=1, activation_type=activation_type)
+        self.conv3 = Conv(hidden_channels * (2 + concat_intermediates * num_bottlenecks), out_channels, 1, stride=1, activation_type=activation_type)
+        module_list = [
+            YoloNASBottleneck(hidden_channels, hidden_channels, block_type, activation_type, shortcut, use_alpha, drop_path_rate=drop_path_rates[i])
+            for i in range(num_bottlenecks)
+        ]
+        self.bottlenecks = SequentialWithIntermediates(concat_intermediates, *module_list)
+        self.dropout = nn.Dropout2d(dropout_rate, inplace=True) if dropout_rate > 0.0 else nn.Identity()
+
+    def forward(self, x: Tensor) -> Tensor:
+        x_1 = self.conv1(x)
+        x_1 = self.bottlenecks(x_1)
+        x_2 = self.conv2(x)
+        x = torch.cat((*x_1, x_2), dim=1)
+        x = self.dropout(x)
+        return self.conv3(x)
+
+
+@register_detection_module()
+class YoloNASStem(BaseDetectionModule, SupportsReplaceInputChannels):
+    """
+    Stem module for YoloNAS. Consists of a single QARepVGGBlock with stride of two.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, stride: int = 2):
+        """
+        Initialize the YoloNASStem module
+        :param in_channels: Number of input channels
+        :param out_channels: Number of output channels
+        """
+        super().__init__(in_channels)
+        self._out_channels = out_channels
+        self.conv = QARepVGGBlock(in_channels, out_channels, stride=stride, use_residual_connection=False)
+
+    @property
+    def out_channels(self):
+        return self._out_channels
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.conv(x)
+
+    def replace_input_channels(self, in_channels: int, compute_new_weights_fn: Optional[Callable[[nn.Module, int], nn.Module]] = None):
+        self.conv = QARepVGGBlock(in_channels, self._out_channels, stride=2, use_residual_connection=False)
+
+    def get_input_channels(self) -> int:
+        return self.conv.in_channels
+
+
+@register_detection_module()
+class YoloNASStage(BaseDetectionModule):
+    """
+    A single stage module for YoloNAS. It consists of a downsample block (QARepVGGBlock) followed by YoloNASCSPLayer.
+    """
+
+    @resolve_param("activation_type", ActivationsTypeFactory())
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        num_blocks: int,
+        activation_type: Type[nn.Module],
+        hidden_channels: int = None,
+        concat_intermediates: bool = False,
+        drop_path_rates: Union[Iterable[float], None] = None,
+        dropout_rate: float = 0.0,
+        stride: int = 2,
+    ):
+        """
+        Initialize the YoloNASStage module
+        :param in_channels: Number of input channels
+        :param out_channels: Number of output channels
+        :param num_blocks: Number of bottleneck blocks in the YoloNASCSPLayer
+        :param activation_type: Activation type for all blocks
+        :param hidden_channels: If not None, sets the number of hidden channels used inside the bottleneck blocks.
+        :param concat_intermediates: If True, concatenates the intermediate values from the YoloNASCSPLayer.
+        :param drop_path_rates: List of drop path probabilities for each bottleneck block.
+                                Must have the length equal to the num_blocks or None.
+        :param dropout_rate: Dropout probability before the last convolution in this layer.
+        """
+        super().__init__(in_channels)
+        self._out_channels = out_channels
+        self.downsample = QARepVGGBlock(in_channels, out_channels, stride=stride, activation_type=activation_type, use_residual_connection=False)
+        self.blocks = YoloNASCSPLayer(
+            out_channels,
+            out_channels,
+            num_blocks,
+            QARepVGGBlock,
+            activation_type,
+            True,
+            hidden_channels=hidden_channels,
+            concat_intermediates=concat_intermediates,
+            drop_path_rates=drop_path_rates,
+            dropout_rate=dropout_rate,
+        )
+
+    @property
+    def out_channels(self):
+        return self._out_channels
+
+    def forward(self, x):
+        return self.blocks(self.downsample(x))
+
+
+@register_detection_module()
+class YoloNASUpStage(BaseDetectionModule):
+    """
+    Upsampling stage for YoloNAS.
+    """
+
+    @resolve_param("activation_type", ActivationsTypeFactory())
+    @resolve_param("upsample_mode", TypeFactory.from_enum_cls(UpsampleMode))
+    def __init__(
+        self,
+        in_channels: List[int],
+        out_channels: int,
+        width_mult: float,
+        num_blocks: int,
+        depth_mult: float,
+        activation_type: Type[nn.Module],
+        hidden_channels: int = None,
+        concat_intermediates: bool = False,
+        reduce_channels: bool = False,
+        drop_path_rates: Union[Iterable[float], None] = None,
+        dropout_rate: float = 0.0,
+        upsample_mode: UpsampleMode = UpsampleMode.CONV_TRANSPOSE,
+    ):
+        """
+        Initialize the YoloNASUpStage module
+        :param in_channels: Number of input channels
+        :param out_channels: Number of output channels
+        :param width_mult: Multiplier for the number of channels in the stage.
+        :param num_blocks: Number of bottleneck blocks
+        :param depth_mult: Multiplier for the number of blocks in the stage.
+        :param activation_type: Activation type for all blocks
+        :param hidden_channels: If not None, sets the number of hidden channels used inside the bottleneck blocks
+        :param concat_intermediates:
+        :param reduce_channels:
+        """
+        super().__init__(in_channels)
+
+        num_inputs = len(in_channels)
+        if num_inputs == 2:
+            in_channels, skip_in_channels = in_channels
+        else:
+            in_channels, skip_in_channels1, skip_in_channels2 = in_channels
+            skip_in_channels = skip_in_channels1 + out_channels  # skip2 downsample results in out_channels channels
+        out_channels = width_multiplier(out_channels, width_mult, 8)
+        num_blocks = max(round(num_blocks * depth_mult), 1) if num_blocks > 1 else num_blocks
+
+        if num_inputs == 2:
+            self.reduce_skip = Conv(skip_in_channels, out_channels, 1, 1, activation_type) if reduce_channels else nn.Identity()
+        else:
+            self.reduce_skip1 = Conv(skip_in_channels1, out_channels, 1, 1, activation_type) if reduce_channels else nn.Identity()
+            self.reduce_skip2 = Conv(skip_in_channels2, out_channels, 1, 1, activation_type) if reduce_channels else nn.Identity()
+
+        self.conv = Conv(in_channels, out_channels, 1, 1, activation_type)
+
+        self.upsample = make_upsample_module_with_explicit_channels(
+            in_channels=out_channels, out_channels=out_channels, scale_factor=2, upsample_mode=upsample_mode, align_corners=True
+        )
+        if num_inputs == 3:
+            self.downsample = Conv(out_channels if reduce_channels else skip_in_channels2, out_channels, kernel=3, stride=2, activation_type=activation_type)
+
+        self.reduce_after_concat = Conv(num_inputs * out_channels, out_channels, 1, 1, activation_type) if reduce_channels else nn.Identity()
+
+        after_concat_channels = out_channels if reduce_channels else out_channels + skip_in_channels
+        self.blocks = YoloNASCSPLayer(
+            after_concat_channels,
+            out_channels,
+            num_blocks,
+            QARepVGGBlock,
+            activation_type,
+            hidden_channels=hidden_channels,
+            concat_intermediates=concat_intermediates,
+            drop_path_rates=drop_path_rates,
+            dropout_rate=dropout_rate,
+        )
+
+        self._out_channels = [out_channels, out_channels]
+
+    @property
+    def out_channels(self):
+        return self._out_channels
+
+    def forward(self, inputs):
+        x, skip_x1, skip_x2 = inputs
+        skip_x1, skip_x2 = self.reduce_skip1(skip_x1), self.reduce_skip2(skip_x2)
+        x_inter = self.conv(x)
+        return x_inter, self.blocks(self.reduce_after_concat(torch.cat([self.upsample(x_inter), *[skip_x1, self.downsample(skip_x2)]], 1)))
+
+
+@register_detection_module()
+class YoloNASDownStage(BaseDetectionModule):
+    @resolve_param("activation_type", ActivationsTypeFactory())
+    def __init__(
+        self,
+        in_channels: List[int],
+        out_channels: int,
+        width_mult: float,
+        num_blocks: int,
+        depth_mult: float,
+        activation_type: Type[nn.Module],
+        hidden_channels: int = None,
+        concat_intermediates: bool = False,
+        drop_path_rates: Union[Iterable[float], None] = None,
+        dropout_rate: float = 0.0,
+    ):
+        """
+        Initializes a YoloNASDownStage.
+
+        :param in_channels: Number of input channels.
+        :param out_channels: Number of output channels.
+        :param width_mult: Multiplier for the number of channels in the stage.
+        :param num_blocks: Number of blocks in the stage.
+        :param depth_mult: Multiplier for the number of blocks in the stage.
+        :param activation_type: Type of activation to use inside the blocks.
+        :param hidden_channels: If not None, sets the number of hidden channels used inside the bottleneck blocks.
+        :param concat_intermediates:
+        """
+
+        super().__init__(in_channels)
+
+        in_channels, skip_in_channels = in_channels
+        out_channels = width_multiplier(out_channels, width_mult, 8)
+        num_blocks = max(round(num_blocks * depth_mult), 1) if num_blocks > 1 else num_blocks
+
+        self.conv = Conv(in_channels, out_channels // 2, 3, 2, activation_type)
+        after_concat_channels = out_channels // 2 + skip_in_channels
+        self.blocks = YoloNASCSPLayer(
+            in_channels=after_concat_channels,
+            out_channels=out_channels,
+            num_bottlenecks=num_blocks,
+            block_type=partial(Conv, kernel=3, stride=1),
+            activation_type=activation_type,
+            hidden_channels=hidden_channels,
+            concat_intermediates=concat_intermediates,
+            drop_path_rates=drop_path_rates,
+            dropout_rate=dropout_rate,
+        )
+
+        self._out_channels = out_channels
+
+    @property
+    def out_channels(self):
+        return self._out_channels
+
+    def forward(self, inputs):
+        x, skip_x = inputs
+        return self.blocks(torch.cat([self.conv(x), skip_x], 1))
