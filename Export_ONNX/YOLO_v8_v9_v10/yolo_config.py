fa9e9f80be22fe5166a07ee055f101604240e03f
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-10-08 18:03:37 +0800

    Delete Export_ONNX/YOLO_v8_v9_v10 directory

--- Export_ONNX/YOLO_v8_v9_v10/yolo_config.py
+++
@@ -1,5 +0,0 @@
-EXPORT_YOLO_VERSION = 10  # Specify the YOLO version to export. The version must match the loaded model.
-EXPORT_YOLO_INPUT_SIZE = [1, 3, 720, 1280]  # Input image shape. Batch size must be 1. Should be a multiple of GPU group (e.g., 16) for optimal efficiency.
-EXPORT_YOLO_RESIZE = (288, 512)  # Resize input to this shape to accelerate YOLO inference. Maintain the same W/H ratio as the input images.
-
-
