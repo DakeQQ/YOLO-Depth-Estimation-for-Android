54aa276c5885a1bdd275d2b7bc7b9c1d90a61162
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-10-08 18:04:03 +0800

    Add files via upload

---
+++ Export_ONNX/YOLO_v8_v9_v10_v11/YOLO_Export.py
@@ -0,0 +1,23 @@
+import shutil
+
+config_path = './yolo_config.py'  # The path where the yolo_config.py stored.
+modified_exporter_path = './modeling_modified/exporter.py'  # The path where the modified exporter.py stored.
+modified_tasks_path = './modeling_modified/tasks.py'  # The path where the modified tasks.py stored.
+ultralytics_engine_path = '/Users/jzhan378/PycharmProjects/pythonProject/.venv/lib/python3.11/site-packages/ultralytics/engine/'  # The original ultralytics/engine path which was stored in the python packages.
+ultralytics_nn_path = '/Users/jzhan378/PycharmProjects/pythonProject/.venv/lib/python3.11/site-packages/ultralytics/nn/'  # The original ultralytics/nn path which was stored in the python packages.
+
+
+# Import the yolo_config
+shutil.copyfile(config_path, ultralytics_engine_path + "yolo_config.py")
+shutil.copyfile(config_path, ultralytics_nn_path + "yolo_config.py")
+
+# Replace the original source code file.
+shutil.copyfile(modified_exporter_path, ultralytics_engine_path + "exporter.py")
+shutil.copyfile(modified_tasks_path, ultralytics_nn_path + "tasks.py")
+
+from ultralytics import YOLO
+# Load a model
+model = YOLO("/Users/jzhan378/Downloads/YOLO/yolo11x.pt")  # Load an official model, or specify your own model path. The version number must match EXPORT_YOLO_VERSION.
+
+# Export the model
+model.export(format='onnx')  # The exported model will save at the current folder.
