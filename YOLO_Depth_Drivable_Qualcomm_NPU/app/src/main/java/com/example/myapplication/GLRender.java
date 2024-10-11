970cc0dbe1f7932e70aae07c2bf592785affa9a6
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-10-11 16:51:44 +0800

    Update GLRender.java

--- YOLO_Depth_Drivable_Qualcomm_NPU/app/src/main/java/com/example/myapplication/GLRender.java
+++ YOLO_Depth_Drivable_Qualcomm_NPU/app/src/main/java/com/example/myapplication/GLRender.java
@@ -160,7 +160,7 @@ public class GLRender implements GLSurfaceView.Renderer {
         if (!run_yolo && !run_depth && !run_twinLite) {
             imageRGBA = Process_Texture();
             executorService.execute(() -> {
-                for (int i = 0; i < imageRGBA.length; i++) {
+                for (int i = 0; i < camera_pixels; i++) {
                     int rgba = imageRGBA[i];
                     image_rgb[i] = (float) ((rgba >> 24) & 0xFF) * inv_255;
                     image_rgb[i + camera_pixels] = (float) ((rgba >> 16) & 0xFF) * inv_255;
