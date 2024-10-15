c21c437292b622e1d283b0fadca7732ef3298288
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-10-15 08:45:54 +0800

    Update YOLO_Export.py

--- Export_ONNX/YOLO_v8_v9_v10_v11/YOLO_Export.py
+++ Export_ONNX/YOLO_v8_v9_v10_v11/YOLO_Export.py
@@ -4,23 +4,23 @@ config_path = './yolo_config.py'  # The path where the yolo_config.py stored.
 modified_exporter_path = './modeling_modified/exporter.py'  # The path where the modified exporter.py stored.
 modified_tasks_path = './modeling_modified/tasks.py'  # The path where the modified tasks.py stored.
 modified_head_path = './modeling_modified/head.py'  # The path where the modified head.py stored.
-ultralytics_engine_path = '/Users/PycharmProjects/.venv/lib/python3.11/site-packages/ultralytics/engine/'  # The original ultralytics/engine path which was stored in the python packages.
-ultralytics_nn_path = '/Users/PycharmProjects/.venv/lib/python3.11/site-packages/ultralytics/nn/'  # The original ultralytics/nn path which was stored in the python packages.
-
+ultralytics_path = '/Users/PycharmProjects/.venv/lib/python3.11/site-packages/ultralytics/'  # The original ultralytics path which was stored in the python packages.
 
 # Import the yolo_config
-shutil.copyfile(config_path, ultralytics_engine_path + "yolo_config.py")
-shutil.copyfile(config_path, ultralytics_nn_path + "yolo_config.py")
-shutil.copyfile(config_path, ultralytics_nn_path + "modules/yolo_config.py")
+shutil.copyfile(config_path, ultralytics_path + "engine/yolo_config.py")
+shutil.copyfile(config_path, ultralytics_path + "nn/yolo_config.py")
+shutil.copyfile(config_path, ultralytics_path + "nn/modules/yolo_config.py")
 
 # Replace the original source code file.
-shutil.copyfile(modified_exporter_path, ultralytics_engine_path + "exporter.py")
-shutil.copyfile(modified_tasks_path, ultralytics_nn_path + "tasks.py")
-shutil.copyfile(modified_head_path, ultralytics_nn_path + "modules/head.py")
+shutil.copyfile(modified_exporter_path, ultralytics_path + "engine/exporter.py")
+shutil.copyfile(modified_tasks_path, ultralytics_path + "nn/tasks.py")
+shutil.copyfile(modified_head_path, ultralytics_path + "nn/modules/head.py")
 
 from ultralytics import YOLO
+
 # Load a model, The version number must match EXPORT_YOLO_VERSION.
 model = YOLO("yolo11n.pt")   # Load an official model,
+
 # model = YOLO("/Users/Downloads/yolo11n.pt") # Or specify your own model path.
 
 # Export the model
