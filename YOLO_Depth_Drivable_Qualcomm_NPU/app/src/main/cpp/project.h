3d83629e801249e7d5385acf709add5a0b920bd7
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-11-06 11:50:30 +0800

    Update project.h

--- YOLO_Depth_Drivable_Qualcomm_NPU/app/src/main/cpp/project.h
+++ YOLO_Depth_Drivable_Qualcomm_NPU/app/src/main/cpp/project.h
@@ -24,7 +24,7 @@ const char* computeShaderSource = "#version 320 es\n"
                                   "    ivec2 texelPos = ivec2(gl_GlobalInvocationID.xy);\n"
                                   "    vec3 rgb = clamp(YUVtoRGBMatrix * (texelFetch(yuvTex, texelPos, 0).rgb + bias), 0.0, 255.0);\n"
                                   // Use int8 packing the pixels, it would be 1.6 times faster than using float32 buffer.
-                                  "    outputData.result[texelPos.y * camera_width + texelPos.x] = (int(rgb.r) << 16) | (int(rgb.g) << 8) | (int(rgb.b));\n"
+                                  "    outputData.result[texelPos.y * camera_width + texelPos.x] = (int(rgb.b) << 16) | (int(rgb.r) << 8) | (int(rgb.g));\n"
                                   "}";
 GLuint pbo_A = 0;
 GLuint computeProgram = 0;
