b2ef00061cbba07a2c94ef3a84d90e79635b2748
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-11-23 13:47:33 +0800

    Update Depth_Export.py

--- Export_ONNX/Depth_Anything_Metric_V2/Depth_Export.py
+++ Export_ONNX/Depth_Anything_Metric_V2/Depth_Export.py
@@ -33,7 +33,7 @@ model.load_state_dict(torch.load(model_path, map_location='cpu'))
 model.to('cpu').eval()
 images = torch.ones(EXPORT_DEPTH_INPUT_SIZE, dtype=torch.float32)
 print("Export Start")
-with torch.no_grad():
+with torch.inference_mode():
     torch.onnx.export(
         model,
         images,
