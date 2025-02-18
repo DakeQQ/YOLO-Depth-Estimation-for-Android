c9c2936dc228db8cd273bccd6f6eedd8a6e9ba22
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-02-18 13:58:26 +0800

    Update YOLO_Export.py

--- Export_ONNX/YOLO_v8_v9_v10_v11/YOLO_Export.py
+++ Export_ONNX/YOLO_v8_v9_v10_v11/YOLO_Export.py
@@ -2,21 +2,17 @@ import shutil
 import torch
 import site
 
-config_path = './yolo_config.py'  # The path where the yolo_config.py stored.
-modified_exporter_path = './modeling_modified/exporter.py'  # The path where the modified exporter.py stored.
-modified_tasks_path = './modeling_modified/tasks.py'  # The path where the modified tasks.py stored.
-modified_head_path = './modeling_modified/head.py'  # The path where the modified head.py stored.
 
 # Import the yolo_config
 ultralytics_path = site.getsitepackages()[-1] + "/ultralytics"
-shutil.copyfile(config_path, ultralytics_path + "/engine/yolo_config.py")
-shutil.copyfile(config_path, ultralytics_path + "/nn/yolo_config.py")
-shutil.copyfile(config_path, ultralytics_path + "/nn/modules/yolo_config.py")
+shutil.copyfile("./yolo_config.py", ultralytics_path + "/engine/yolo_config.py")
+shutil.copyfile("./yolo_config.py", ultralytics_path + "/nn/yolo_config.py")
+shutil.copyfile("./yolo_config.py", ultralytics_path + "/nn/modules/yolo_config.py")
 
 # Replace the original source code file.
-shutil.copyfile(modified_exporter_path, ultralytics_path + "/engine/exporter.py")
-shutil.copyfile(modified_tasks_path, ultralytics_path + "/nn/tasks.py")
-shutil.copyfile(modified_head_path, ultralytics_path + "/nn/modules/head.py")
+shutil.copyfile("./modeling_modified/exporter.py", ultralytics_path + "/engine/exporter.py")
+shutil.copyfile("./modeling_modified/tasks.py", ultralytics_path + "/nn/tasks.py")
+shutil.copyfile("./modeling_modified/head.py", ultralytics_path + "/nn/modules/head.py")
 
 from ultralytics import YOLO
 
