61fc976bc964e7ce8fd8576dc29c558efe25a1f3
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-03-24 09:35:15 +0800

    Add files via upload

---
+++ Export_ONNX/YOLO_v8_v9_v10_v11_v12/YOLO_Export.py
@@ -0,0 +1,24 @@
+import shutil
+import torch
+import site
+
+# Import the yolo_config
+python_package_path = site.getsitepackages()[-1]
+shutil.copyfile("./yolo_config.py", python_package_path + "/ultralytics/engine/yolo_config.py")
+shutil.copyfile("./yolo_config.py", python_package_path + "/ultralytics/nn/yolo_config.py")
+shutil.copyfile("./yolo_config.py", python_package_path + "/ultralytics/nn/modules/yolo_config.py")
+
+# Replace the original source code file.
+shutil.copyfile("./modeling_modified/exporter.py", python_package_path + "/ultralytics/engine/exporter.py")
+shutil.copyfile("./modeling_modified/tasks.py", python_package_path + "/ultralytics/nn/tasks.py")
+shutil.copyfile("./modeling_modified/head.py", python_package_path + "/ultralytics/nn/modules/head.py")
+
+from ultralytics import YOLO
+
+# Load a model, The version number must match EXPORT_YOLO_VERSION.
+model = YOLO("yolo12n.pt")                                 # Load an official model,
+# model = YOLO("/Users/Downloads/custom_yolo12n.pt")       # Or specify your own model path. Currently, do not support yolo12-turbo series.
+
+# Export the model
+with torch.inference_mode():
+    model.export(format='onnx')                            # The exported model will save at the current folder.
