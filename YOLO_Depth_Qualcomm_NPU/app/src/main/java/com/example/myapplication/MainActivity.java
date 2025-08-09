f7e77e9c6cd3b23e46d4011754217f36c4baee71
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-08-09 13:06:48 +0800

    Update MainActivity.java

--- YOLO_Depth_Qualcomm_NPU/app/src/main/java/com/example/myapplication/MainActivity.java
+++ YOLO_Depth_Qualcomm_NPU/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -356,7 +356,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static native void Process_Init(int textureId);
-    public static native int[] Process_Texture();
+    public static native void Process_Texture(byte[] pixel_values);
     private native boolean Load_Models_A(AssetManager assetManager, boolean USE_FLOAT_MODEL, boolean USE_QNN, boolean USE_DSP_NPU);
     private native boolean Load_Models_B(AssetManager assetManager, boolean USE_FLOAT_MODEL, boolean USE_QNN, boolean USE_DSP_NPU);
     public static native float[] Run_YOLO(byte[] pixel_values);
